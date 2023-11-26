484096c932ae343ca33c71c243d836a200147d7e
Homero Cabrera Araque <homero9726@gmail.com>
2023-11-26 02:21:31 -0500

    new config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/home",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
