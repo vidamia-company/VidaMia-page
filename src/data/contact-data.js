cd28754217ddf59ed992618f4e2b47ee5d04a304
Homero Cabrera Araque <homero9726@gmail.com>
2023-11-30 22:43:08 -0500

    Update contact data icons and descriptions

--- src/data/contact-data.js
+++ src/data/contact-data.js
@@ -1,29 +1,44 @@
 import {
-  BriefcaseIcon,
+  CogIcon,
   ChartBarIcon,
   PlayIcon,
-  ArrowDownRightIcon
+  CubeTransparentIcon,
+  ClipboardDocumentCheckIcon,
+  BoltIcon
 } from "@heroicons/react/24/solid";
 
 export const contactData = [
   {
-    title: "Automatizacion de procesos",
-    icon: BriefcaseIcon,
-    description:
-      "generamae una descripcion",
+    title: "Automatización de Procesos",
+    icon: CogIcon,
+    description: "Optimizamos tus operaciones mediante la automatización de procesos. Simplifica tareas, mejora la eficiencia y potencia tu productividad.",
   },
   {
-    title: "Grow Your Market",
+    title: "Crecimiento de Mercado",
     icon: ChartBarIcon,
-    description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    description: "Impulsa tu mercado con estrategias efectivas. Aprovecha nuestro expertise para hacer crecer tu presencia y atraer a nuevos clientes.",
   },
   {
-    title: "Launch Time",
+    title: "Lanzamiento Rápido",
     icon: PlayIcon,
-    description:
-      "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    description: "Acelera tu camino al éxito con Vidamia. Garantizamos un lanzamiento rápido y efectivo de tus proyectos, llevándolos al siguiente nivel.",
   },
+  {
+    title: "Innovación Tecnológica",
+    icon: BoltIcon,
+    description: "Mantenemos tu empresa a la vanguardia mediante la implementación de las últimas innovaciones tecnológicas. Prepárate para el futuro con Vidamia.",
+  },
+  {
+    title: "Desarrollo de Software Personalizado",
+    icon: CubeTransparentIcon,
+    description: "Creamos soluciones de software a medida para satisfacer tus necesidades específicas. Desde la conceptualización hasta la implementación, estamos contigo en cada paso.",
+  },
+  {
+    title: "Auditoría y Mejora Continua",
+    icon: ClipboardDocumentCheckIcon,
+    description: "Evaluamos y optimizamos tus procesos para garantizar eficiencia y calidad. Nuestra auditoría y enfoque de mejora continua elevan tu rendimiento empresarial.",
+  },
+  // Puedes agregar más servicios si es necesario
 ];
 
 export default contactData;
